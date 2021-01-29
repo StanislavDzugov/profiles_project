57478ff3f30ff86b1d87c86b21fe60d5aabd88d6
Stanislav Dzugov <dzougov91@gmail.com>
2021-01-29 22:05:48 +0300

    Permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from profiles_api import serializers
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
-# Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
