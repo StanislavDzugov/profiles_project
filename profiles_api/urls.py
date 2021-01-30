f2662d97726a17e5c121c60e2939fdf9a096bcdc
Stanislav Dzugov <dzougov91@gmail.com>
2021-01-30 11:30:16 +0300

    Profile Feed

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,6 +4,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
